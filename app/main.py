e4725b6a5a1da4c679831d7de6fbf623776cc76b
Annareddy Jagadeswar Reddy <100749580+ANNAREDDY123@users.noreply.github.com>
2026-08-13 14:26:44 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 
+from routes.auth import router as auth_router
 from routes.farms import router as farm_router
 
 app = FastAPI(
     title="Smart Agriculture & Farm Management System"
 )
 
+app.include_router(auth_router)
 app.include_router(farm_router)
 
 
