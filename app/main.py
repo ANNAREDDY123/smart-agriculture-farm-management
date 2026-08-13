1e210a1a742582c5f2dc13e9878460b737d56767
Annareddy Jagadeswar Reddy <100749580+ANNAREDDY123@users.noreply.github.com>
2026-08-13 16:08:58 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from routes.auth import router as auth_router
 from routes.farms import router as farm_router
 from routes.fields import router as field_router
 from routes.crops import router as crop_router
+from routes.irrigation import router as irrigation_router
 
 
 app = FastAPI(
@@ -15,6 +16,7 @@ app.include_router(auth_router)
 app.include_router(farm_router)
 app.include_router(field_router)
 app.include_router(crop_router)
+app.include_router(irrigation_router)
 
 
 @app.get("/")
